567dd80c11ec3b2efc4919bab46b359b8896137c
mariorosenthal <mario@digitalplatinum.io>
2018-11-15 04:13:51 -0500

    testing for stupid reasons

--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,170 @@
 
 Route::get('/', function () {
     $artistsobject = DB::table('artists')->get();
+    $artistsobject = [
+        0=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        1=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        2=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        3=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        4=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        5=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        6=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        7=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ],
+        8=>[
+            'id' => 1,
+            'first_name' => 'jeff',
+            'last_name' => 'mor',
+            'picture' => 'jeffpic.jpg',
+            'bio' => 'STRANGE PEOPLE// EARLY MORNING MUSIC // SAGOL // RITMIK RECORDS
+
+A dedicated digger of the abstract and obscure, the concept of maintaining consistency in groove is something Jeff Moreno has mastered. Carving up complex rhythms for serval years now, he has released music on the likes of labels such as Early Morning Music, Ritmik, and Sagol. In 2018 Jeff Moreno has teamed up with long time friend and label mate Michael Rosa to launch their own signature imprint, Strange People. A celebration of audio counter culture pushing the mysterious and mischievous side of electronic music...',
+            'facebook' => 'https://www.facebook.com/subdmusic',
+            'twitter' => '',
+            'soundcloud' => ''
+        ]
+    ];
     foreach ($artistsobject as $key => $artist)
     {
         $artists[$key] = [
-            'id' => $artist->id,
-            'first_name' => $artist->first_name,
-            'last_name' => $artist->last_name,
-            'picture' => $artist->picture,
-            'bio' => $artist->bio,
-            'facebook' => $artist->facebook,
-            'twitter' => $artist->twitter,
-            'soundcloud' => $artist->soundcloud
+//            'id' => $artist->id,
+//            'first_name' => $artist->first_name,
+//            'last_name' => $artist->last_name,
+//            'picture' => $artist->picture,
+//            'bio' => $artist->bio,
+//            'facebook' => $artist->facebook,
+//            'twitter' => $artist->twitter,
+//            'soundcloud' => $artist->soundcloud
+            'id' => $artist['id'],
+            'first_name' => $artist['first_name'],
+            'last_name' => $artist['last_name'],
+            'picture' => $artist['picture'],
+            'bio' => $artist['bio'],
+            'facebook' => $artist['facebook'],
+            'twitter' => $artist['twitter'],
+            'soundcloud' => $artist['soundcloud']
         ] ;
     }
     shuffle($artists);
-    $eventsobject = DB::table('events')->get();
+//    $eventsobject = DB::table('events')->get();
+    $eventsobject =  [
+        0=>[
+            'id' => 1,
+            'day' => '17',
+            'month' => 'mor',
+            'year' => '2018',
+            'city' => 'miami',
+            'state' => 'asdfasdf',
+            'name' => 'asdfsfd',
+            'venue' => 'asdfasfd',
+        ]
+];
     foreach ($eventsobject as $key => $event)
     {
         $events[$key] = [
-            'id' => $event->id,
-            'day' => $event->day,
-            'month' => $event->month,
-            'year' => $event->year,
-            'city' => $event->city,
-            'state' => $event->state,
-            'name' => $event->name,
-            'venue' => $event->venue
+//            'id' => $event->id,
+//            'day' => $event->day,
+//            'month' => $event->month,
+//            'year' => $event->year,
+//            'city' => $event->city,
+//            'state' => $event->state,
+//            'name' => $event->name,
+//            'venue' => $event->venue
+            'id' => $event['id'],
+            'day' => $event['day'],
+            'month' => $event['month'],
+            'year' => $event['year'],
+            'city' => $event['city'],
+            'state' => $event['state'],
+            'name' => $event['name'],
+            'venue' => $event['venue']
         ] ;
     }
     array_reverse($events);
