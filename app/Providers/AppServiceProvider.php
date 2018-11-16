0f1a95a5c0ccef1816036a86d30e980ffc8e1bf9
mariorosenthal <mario@digitalplatinum.io>
2018-11-16 01:36:57 -0500

    added artist page and fixed minor details

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 
