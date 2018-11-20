ef38db23250a58e34521f0cf6b8150d90e0b6bec
mariorosenthal <mario@digitalplatinum.io>
2018-11-19 19:42:43 -0500

    turing off https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceScheme('https');
-        }
+//        if(env('APP_ENV') !== 'local')
+//        {
+//            $url->forceScheme('https');
+//        }
     }
 
     /**
